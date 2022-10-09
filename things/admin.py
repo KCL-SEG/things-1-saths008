3336715035acccb6434b994c2c17f87eb325e024
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2022-10-09 16:21:34 +0100

    Add Thing model

--- things/admin.py
+++ things/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Thing
 # Register your models here.
+
+@admin.register(Thing)
+class ThingAdmin(admin.ModelAdmin):
+	
+	list_display = ['name', 'description', 'quantity',]
--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class Thing(AbstractUser):
+	name = models.TextField()
+	description = models.TextField()
+	quantity = models.IntegerField()
