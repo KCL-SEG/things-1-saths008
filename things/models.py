b30a462602fd2d934824508666c149a47dbc6be4
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2022-10-22 21:23:41 +0100

    Rearrange order of options in fields

--- things/models.py
+++ things/models.py
@@ -6,7 +6,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Thing(models.Model):
-	name = models.CharField(unique = True, blank = False, max_length = 30)
-	description = models.CharField(blank = True, max_length = 120)
-	quantity = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(100)] )
+	name = models.CharField(max_length = 30, unique = True, blank = False)
+	description = models.CharField(max_length = 120, blank = True)
+	quantity = models.IntegerField(
+		validators = [MinValueValidator(0), MaxValueValidator(100)]
+		 )
 	
