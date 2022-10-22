f39a2aaf5a9c5c7b18c708a2a75a219530ff65a8
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2022-10-22 21:33:55 +0100

    Add unique field option to quantity

--- things/models.py
+++ things/models.py
@@ -7,8 +7,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
 	name = models.CharField(max_length = 30, unique = True, blank = False)
-	description = models.CharField(max_length = 120, blank = True)
+	description = models.CharField(max_length = 120, unique = False, blank = True)
 	quantity = models.IntegerField(
+		unique = False,
 		validators = [MinValueValidator(0), MaxValueValidator(100)]
 		 )
 	
